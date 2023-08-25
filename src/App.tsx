72895dc18a85c170c37f84ae00f8ca1795969eb8
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-08-25 14:12:26 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Heading from './components/Heading';
+import Section from './components/Section';
+
+function App() {
+  return (
+    <>
+      <Heading title="Hello!" />
+      <Section>This is section description</Section>
+    </>
+  );
+}
+
+export default App;
