779e64c5de7d586ce250669ac1252673b93c30c3
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-08-25 19:35:38 +0300

    useState Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import Button from './components/Button';
 import Heading from './components/Heading';
 import Section from './components/Section';
+import { AuthUser } from './components/types/User.types';
 
 function App() {
+  const [user, setUSer] = useState<AuthUser | null>(null);
+
   const handleChange = (e: ChangeEvent): void => {
     console.log(e);
   };
@@ -18,6 +21,19 @@ function App() {
         }}
       />
       <input onChange={(e) => handleChange(e)} />
+      <br />
+      <Button
+        handleClick={() => {
+          setUSer({
+            name: 'Ahmed',
+            email: 'ahmed@gmail.com',
+          });
+        }}
+      />
+      <div>
+        <p>User: {user?.name}</p>
+        <p>Email: {user?.email}</p>
+      </div>
     </>
   );
 }
